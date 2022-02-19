82cb2f8bc13f2135971acb4b10c51532ea963065
geisoft <cristian.andreon@geisoft.com>
2022-02-19 13:50:14 +0100

    +

---
+++ src/com/liquid/FileTail.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.liquid;
+
+import org.json.JSONException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspWriter;
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+class FileTail extends Thread {
+
+    public String fileName;
+    public boolean keepReadingTail = true;
+    public OutputStream outputStream = null;
+    public HttpServletRequest request = null;
+    public String token = null;
+    public String error = null;
+
+    static FileTail glFileTail = null;
+
+    /**
+     * Start single instance listem thread to file content
+     *
+     * @param outputStream
+     * @param request
+     * @param jspWriter
+     * @param token
+     * @return
+     */
+    public static boolean start_tail_file(OutputStream outputStream, HttpServletRequest request, JspWriter jspWriter, String token) {
+        if(outputStream != null && request != null) {
+            if(glFileTail != null) {
+                stop_tail_file(outputStream, request, jspWriter, token);
+                glFileTail = null;
+            }
+            if(glFileTail == null) {
+                FileTail fileTail = new FileTail();
+                glFileTail = fileTail;
+                fileTail.fileName = request.getParameter("fileName");
+                fileTail.outputStream = outputStream;
+                fileTail.request = request;
+                fileTail.token = token;
+                fileTail.keepReadingTail = true;
+
+                // Avvio del thread
+                fileTail.start();
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean stop_tail_file(OutputStream outputStream, HttpServletRequest request, JspWriter jspWriter, String token) {
+        if (glFileTail != null) {
+            glFileTail.keepReadingTail = false;
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            glFileTail.stop();
+            glFileTail = null;
+            return true;
+        }
+        return false;
+    }
+
+    public void run() {
+        try {
+
+            this.error = null;
+
+            File f = new File(fileName);
+            BufferedReader inStream = new BufferedReader( new InputStreamReader( new FileInputStream(f) ));
+            boolean retVal = false;
+            int lineCount = 0;
+
+            if (inStream != null) {
+
+                // go to EOF
+                while (true) {
+                    if (inStream.ready()) {
+                        inStream.readLine();
+                    } else {
+                        break;
+                    }
+                }
+
+                String line = null;
+                while (keepReadingTail) {
+                    if (inStream.ready()) {
+                        line = inStream.readLine();
+                        lineCount++;
+                        if (line != null) {
+                            if(outputStream != null) {
+                                wsStreamerClient.send(outputStream, line, token);
+                            }
+                        }
+                    } else {
+                        Thread.sleep(1000);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            Logger.getLogger(utility.class.getName()).log(Level.SEVERE, null, e);
+            error = e.getMessage();
+            keepReadingTail = false;
+        }
+    }
+}
